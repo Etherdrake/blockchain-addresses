be11f94fe5a113de6a7b3fc99d6ca77a2cc43e25
Etherdrake <67021215+Etherdrake@users.noreply.github.com>
2024-02-22 09:06:39 -0500

    Added a few new tokens

--- src/arbitrum/tokens.rs
+++ src/arbitrum/tokens.rs
@@ -1,3 +1,6 @@
 pub const WETH: &str = "0x82af49447d8a07e3bd95bd0d56f35241523fbab1";
 pub const WBTC: &str = "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f";
 pub const LINK: &str = "0xf97f4df75117a78c1a5a0dbb814af92458539fb4";
+pub const GNS: &str = "0x18c11fd286c5ec11c3b683caa813b77f5163a122";
+pub const ARB: &str = "0x912ce59144191c1204e64559fe8253a0e49e6548";
+pub const AXL: &str = "0x23ee2343b892b1bb63503a4fabc840e0e2c6810f";
--- src/polygon/tokens.rs
+++ src/polygon/tokens.rs
@@ -1,3 +1,5 @@
 pub const WETH: &str = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
 pub const LINK: &str = "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39";
 pub const WBTC: &str = "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6";
+pub const GNS: &str = "0xe5417af564e4bfda1c483642db72007871397896";
+pub const AXL: &str = "0x6e4e624106cb12e168e6533f8ec7c82263358940";
